f2bbf79c062a06002825c9cfb2791795b23089d6
aburrell <ab763@le.ac.uk>
2017-08-02 15:26:33 -0500

    mock update
    Fixed bug in conf file where sys is needed

--- docs/conf.py
+++ docs/conf.py
@@ -16,14 +16,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 import mock
 
-MOCK_MODULES = ['datetime', 'numpy', 'logging', 'os', 'setuptools',
-                '__future__', 'filecmp', 'unittest']
+MOCK_MODULES = ['datetime', 'numpy', 'logging', 'setuptools', '__future__',
+                'filecmp', 'unittest']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
