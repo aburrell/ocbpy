ef730acf0cdd37735b08b127bdfbe4ee3f4963a8
aburrell <ab763@le.ac.uk>
2017-12-20 14:02:09 -0600

    Fixed dependencies
    Added new required and recommended dependencies to setup.py

--- setup.py
+++ setup.py
@@ -48,4 +48,6 @@ setup(name='ocbpy',
       include_package_data=True,
       zip_safe=False,
       test_suite='setup.ocb_test_suite',
+      install_requires=['h5py'],
+      extras_require={'davitpy':['https://github.com/vtsuperdarn/davitpy.git'],}
 )
