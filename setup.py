dfce3bde8bbc8e9f02c466521ac6894b2122b90e
aburrell <ab763@le.ac.uk>
2018-03-19 14:29:51 -0500

    version specific kwargs
    Added version specific kwargs to reading

--- setup.py
+++ setup.py
@@ -7,11 +7,17 @@
 from __future__ import absolute_import
 from os import path
 from setuptools import setup, find_packages
+from sys import version_info
 
 # Define a read function for using README for long_description
 
 def read(fname, **kwargs):
     return open(path.join(path.dirname(__file__), fname), **kwargs).read()
+
+# Define default kwargs for python2/3
+read_kwargs = dict()
+if version_info.major == 3:
+    read_kwargs = {"encoding", "utf8"} 
 
 # Define a test suite
 
@@ -31,7 +37,7 @@ setup(name='ocbpy',
       author='Angeline G. Burrell',
       author_email='agb073000@utdallas.edu',
       description='Location relative to open/closed field line boundary',
-      long_description=read('README.md'),
+      long_description=read('README.md', read_kwargs),
       packages=find_packages(),
       classifiers=[
           "Development Status :: 3 - Alpha",
