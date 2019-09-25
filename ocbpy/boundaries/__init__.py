16ac543a0833e3cb48dc479e03fb2db000164f7f
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2019-09-25 13:47:53 -0400

    ENH: files import
    Import boundaries.files routines into ocbpy.

---
+++ ocbpy/boundaries/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019, AGB * GC
+# Full license can be found in LICENSE.txt
+"""Boundary file utilities
+
+Contains
+-------------------------------------------------------------------------------
+files   Boundary file utilities
+-------------------------------------------------------------------------------
+
+"""
+from __future__ import (absolute_import)
+
+from . import (files)
