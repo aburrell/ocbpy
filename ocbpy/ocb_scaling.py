BUG: fixed bug in north sign
Fixed bug in northern sign assignment.  Also moved all imports to top.

ocbpy/ocb_scaling.py
@@ -255,10 +255,6 @@ class VectorData(object):
             mesurement OCB latitude (degrees)]
             Not necessary if no magnitude scaling is needed. (default=None)
 
-        Returns
-        --------
-            self : Initialised VectorData class object by setting AACGM values
-
         """
         # Assign the vector data name and units
         self.dat_name = dat_name
@@ -399,7 +395,6 @@ class VectorData(object):
             Not necessary if defined earlier or if no scaling is needed.
 
         """
-        from ocbpy.ocb_time import deg2hr
 
         # If the OCB vector coordinates weren't included in the initial info,
         # update them here
@@ -416,7 +411,7 @@ class VectorData(object):
         # Set the AACGM coordinates of the OCB pole
         self.unscaled_r = ocb.r[self.ocb_ind] + self.r_corr
         self.scaled_r = 90.0 - abs(ocb.boundary_lat)
-        self.ocb_aacgm_mlt = deg2hr(ocb.phi_cent[self.ocb_ind])
+        self.ocb_aacgm_mlt = ocbpy.ocb_time.deg2hr(ocb.phi_cent[self.ocb_ind])
         self.ocb_aacgm_lat = 90.0 - ocb.r_cent[self.ocb_ind]
 
         # Get the angle at the data vector appended by the AACGM and OCB poles
@@ -724,15 +719,15 @@ class VectorData(object):
             
             if(quads[1][1] or quads[2][2] or quads[3][3] or quads[4][4] or
                (quads[1][4] and self.aacgm_naz <= pole_plus) or
-               (quads[1][2] and self.aacgm_naz > minus_pole) or
-               (quads[2][1] and self.aacgm_naz <= minus_pole) or 
-               ((quads[3][4] or quads[4][3]) and self.aacgm_naz <= pole_minus)
-               or ((quads[3][2] or quads[4][1]) and self.aacgm_naz > pole_minus)
+               ((quads[1][2] or quads[2][1])
+                and self.aacgm_naz <= minus_pole) or
+               ((quads[3][4] or quads[4][3])
+                and self.aacgm_naz <= pole_minus) or
+               ((quads[3][2] or quads[4][1]) and self.aacgm_naz > pole_minus)
                or (quads[2][3] and self.aacgm_naz > minus_pole)):
                 vsigns["north"] = 1
-            elif((quads[1][2] and self.aacgm_naz > minus_pole) or
-                 (quads[1][4] and self.aacgm_naz > pole_plus) or
-                 (quads[2][1] and self.aacgm_naz > minus_pole) or
+            elif(((quads[1][2] or quads[2][1]) and self.aacgm_naz > minus_pole)
+                 or (quads[1][4] and self.aacgm_naz > pole_plus) or
                  ((quads[4][1] or quads[3][2]) and self.aacgm_naz <= pole_minus)
                  or (quads[2][3] and self.aacgm_naz <= minus_pole) or
                  ((quads[4][3] or quads[3][4]) and self.aacgm_naz > pole_minus)
@@ -782,7 +777,6 @@ class VectorData(object):
             Angle in degrees between AACGM north, a measurement, and OCB north
 
         """
-        from ocbpy.ocb_time import hr2rad
         
         # Test input
         if np.isnan(self.aacgm_mlt):
@@ -799,7 +793,7 @@ class VectorData(object):
 
         # Convert the AACGM MLT of the observation and OCB pole to radians,
         # then calculate the difference between them.
-        del_long = hr2rad(self.ocb_aacgm_mlt - self.aacgm_mlt)
+        del_long = ocbpy.ocb_time.hr2rad(self.ocb_aacgm_mlt - self.aacgm_mlt)
 
         if del_long < 0.0:
             del_long += 2.0 * np.pi