2bdf93468fa0ccea70d9b9d0e118a9b950e4cb1c
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-08-05 09:25:26 -0400

    MAINT: PEP8 and version number
    Updated version number and made PEP8 and style changes to docstrings.

--- ocbpy/__init__.py
+++ ocbpy/__init__.py
@@ -2,47 +2,42 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019, AGB & GC
 # Full license can be found in License.md
-#-----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 """
-ocboundary
------------
-
 Open-Closed field line Boundary (OCB) magnetic gridding
 
 Functions
----------------------------------------------------------------------------
+---------
 match_data_ocb      Matches data and OCB records
 normal_evar         Normalise a variable proportional to the electric field
 normal_curl_evar    Normalise a variable proportional to the curl of the
                     electric field
----------------------------------------------------------------------------
 
 Classes
----------------------------------------------------------------------------
+-------
 OCBoundary    OCB data for different times
 VectorData    Vector data point
----------------------------------------------------------------------------
 
 Modules
----------------------------------------------------------------------------
+-------
 boundaries     Boundary file utilities
 instruments    Instrument-specific OCB gridding functions
 ocb_time       Time manipulation routines
 ocb_scaling    Scaling functions for OCB gridded data
 ocb_correction Boundary correction utilities
----------------------------------------------------------------------------
 
 """
-from __future__ import (absolute_import, unicode_literals)
+
+from __future__ import absolute_import, unicode_literals
 
 import logging
 
-from ocbpy import (ocboundary, ocb_scaling, ocb_time, ocb_correction)
-from ocbpy.ocboundary import (OCBoundary, match_data_ocb)
-from ocbpy import (instruments, boundaries)
+from ocbpy import ocboundary, ocb_scaling, ocb_time, ocb_correction
+from ocbpy.ocboundary import OCBoundary, match_data_ocb
+from ocbpy import instruments, boundaries
 
 # Define the global variables
-__version__ = str('0.2b2')
+__version__ = str('0.2.0')
 
 # Define a logger object to allow easier log handling
 logging.raiseExceptions = False
