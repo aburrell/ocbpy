7052182df69c07d8ba439f4f134cf4f2b4d74776
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-06-25 08:09:11 -0400

    ENH: add logo to html sidebar
    Change html theme and add the logo to the sidebar.

--- docs/conf.py
+++ docs/conf.py
@@ -94,13 +94,16 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = os.path.join(os.path.abspath('.'), 'figures', 'ocbpy_logo.gif')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+html_theme_options = {'logo_only': True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
