4d9cc4822c360803a7c07b3786256c05de899827
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-12-07 07:16:07 -0500

    MAINT: updated interspinx link
    Updated the intersphinx inventory link.

--- docs/conf.py
+++ docs/conf.py
@@ -178,4 +178,4 @@ epub_copyright = copyright
 epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/3': None}
