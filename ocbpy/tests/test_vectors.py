539e79bbd45ebdfec023145a9db0a9e7f7ad623e
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2024-04-23 11:49:53 -0400

    TST: added unit tests for `define_pole_quadrants`
    Added unit tests for `define_pole_quadrants` and simplified unit tests by making some of the variables class variables.

--- ocbpy/tests/test_vectors.py
+++ ocbpy/tests/test_vectors.py
@@ -16,13 +16,19 @@ class TestOCBVectors(unittest.TestCase):
 
     def setUp(self):
         """Initialize the test class."""
+        self.lt = [0.0, 24.0, 6.0, 21.22, 22.0, 6.0, 6.0, 0.0]
+        self.lat = [50.0, 50.0, 50.0, 87.2, 75.0, 88.62, 87.24, 89.0]
+        self.pole_lt = [0.0, 12.0, 18.0, 1.260677777, 5.832, 6.0, 0.0, 22.0]
+        self.pole_lat = [88.0, 88.0, 88.0, 83.99, 87.24, 87.24, 87.24, 85.0]
+        self.pole_ang = [0.0, 0.0, 0.0, 91.72024697182087, 8.67527923, 180.0,
+                         45.03325090532819, 143.11957692472973]
         self.out = None
         self.comp = None
         return
 
     def tearDown(self):
         """Clean up the test class."""
-        del self.out, self.comp
+        del self.lt, self.lat, self.out, self.comp
         return
 
     def test_get_pole_loc_float(self):
@@ -62,44 +68,36 @@ class TestOCBVectors(unittest.TestCase):
         """Test angle calc between base pole, data, and new pole for floats."""
         # Set locations designed to test no angle, flat lines, acute angles,
         # isocelese triangles, and oblique angles
-        lt = [0.0, 24.0, 6.0, 21.22, 22.0, 6.0, 6.0, 4.832]
-        lat = [50.0, 50.0, 50.0, 87.2, 75.0, 88.62, 87.24, 88.62]
-        pole_lt = [0.0, 12.0, 18.0, 1.260677777, 5.832, 6.0, 0.0, 5.832]
-        pole_lat = [88.0, 88.0, 88.0, 83.99, 87.24, 87.24, 87.24, 87.24]
-        self.comp = [0.0, 0.0, 0.0, 91.72024697182087, 8.67527923, 180.0,
-                     45.03325090532819, 150.9561733411]
-
         for hemi in [-1, 1]:
-            for i, cval in enumerate(self.comp):
+            for i, self.comp in enumerate(self.pole_ang):
                 # Set the input arguments
-                args = [lt[i], hemi * lat[i], pole_lt[i], hemi * pole_lat[i]]
+                args = [self.lt[i], hemi * self.lat[i], self.pole_lt[i],
+                        hemi * self.pole_lat[i]]
 
                 with self.subTest(args=args):
                     # Calculate the pole angle
                     self.out = vectors.calc_vec_pole_angle(*args)
 
                     # Test the output
-                    self.assertAlmostEqual(self.out, cval)
+                    self.assertAlmostEqual(self.out, self.comp)
         return
 
     def test_calc_vec_pole_angle_array(self):
         """Test angle calc between base pole, data, and new pole for arrays."""
         # Set locations designed to test no angle, flat lines, acute angles,
         # isocelese triangles, and oblique angles
-        lt = np.array([0.0, 24.0, 6.0, 21.22, 22.0, 6.0, 6.0, 4.832])
-        lat = np.array([50.0, 50.0, 50.0, 87.2, 75.0, 88.62, 87.24, 88.62])
-        pole_lt = np.array([0.0, 12.0, 18.0, 1.260677777, 5.832, 6.0, 0.0,
-                            5.832])
-        pole_lat = np.array([88.0, 88.0, 88.0, 83.99, 87.24, 87.24, 87.24,
-                             87.24])
-        self.comp = np.array([0.0, 0.0, 0.0, 91.72024697182087, 8.67527923,
-                              180.0, 45.03325090532819, 150.9561733411])
+        self.lt = np.asarray(self.lt)
+        self.lat = np.asarray(self.lat)
+        self.pole_lt = np.asarray(self.pole_lt)
+        self.pole_lat = np.asarray(self.pole_lat)
+        self.comp = np.array(self.pole_ang)
 
         for hemi in [-1, 1]:
             with self.subTest(hemi=hemi):
                 # Calculate the pole angle
                 self.out = vectors.calc_vec_pole_angle(
-                    lt, hemi * lat, pole_lt, hemi * pole_lat)
+                    self.lt, hemi * self.lat, self.pole_lt,
+                    hemi * self.pole_lat)
 
                 # Test the output
                 self.assertTrue(np.all(abs(self.out - self.comp) < 1e-5),
@@ -122,10 +120,115 @@ class TestOCBVectors(unittest.TestCase):
                 self.out = vectors.calc_vec_pole_angle(*args)
 
                 # Test the output
-                self.assertTrue(self.out.shape == self.comp.shape)
+                self.assertTupleEqual(self.out.shape, self.comp.shape)
                 self.assertTrue(np.all(abs(self.out - self.comp) < 1e-5),
                                 msg="{:} != {:}".format(self.out, self.comp))
         return
-        
-                     
+
+    def test_define_pole_quadrants_float(self):
+        """Test the LT quadrant ID for the destination pole for floats."""
+        # Set the expected pole quadrant output
+        self.comp = [1, 2, 2, 4, 1, 4, 2, 3]
+
+        # Cycle through each of the local time pairs
+        for i, data_lt in enumerate(self.lt):
+            # Set the function arguements
+            args = [data_lt, self.pole_lt[i], self.pole_ang[i]]
+
+            with self.subTest(args=args):
+                # Get the output
+                self.out = vectors.define_pole_quadrants(*args)
+
+                # Test the integer quadrant assignment
+                self.assertEqual(self.out, self.comp[i])
+        return
+
+    def test_define_pole_quadrants_array(self):
+        """Test the LT quadrant ID for the destination pole for arrays."""
+        # Set the expected pole quadrant output
+        self.comp = np.array([1, 2, 2, 4, 1, 4, 2, 3])
+
+        # Cycle through list-like or array-like inputs
+        for is_array in [True, False]:
+            # Set the function arguements
+            args = [self.lt, self.pole_lt, self.pole_ang]
+
+            if is_array:
+                for i, arg in enumerate(args):
+                    args[i] = np.asarray(arg)
+
+            with self.subTest(is_array=is_array):
+                # Get the output
+                self.out = vectors.define_pole_quadrants(*args)
+
+                # Test the integer quadrant assignment
+                self.assertTupleEqual(self.out.shape, self.comp.shape)
+                self.assertTrue(np.all(self.out == self.comp),
+                                msg="{:} != {:}".format(self.out, self.comp))
+        return
+
+    def test_define_pole_quadrants_mixed(self):
+        """Test the LT quadrant ID for the destination pole for mixed input."""
+        # Cycle through the mixed inputs
+        for args, self.comp in [
+                ([self.lt[0], np.asarray(self.pole_lt)[:2],
+                  np.asarray(self.pole_ang)[:2]], np.array([1, 2])),
+                ([np.asarray(self.lt)[:2], self.pole_lt[0], self.pole_ang[0]],
+                 np.array([1, 1]))]:
+            with self.subTest(args=args):
+                # Get the output
+                self.out = vectors.define_pole_quadrants(*args)
+
+                # Test the integer quadrant assignment
+                self.assertTupleEqual(self.out.shape, self.comp.shape)
+                self.assertTrue(np.all(self.out == self.comp),
+                                msg="{:} != {:}".format(self.out, self.comp))
+        return
+
+    def test_define_pole_quadrants_neg_diff(self):
+        """Test the LT quadrant ID for the dest pole with very neg LT diffs."""
+        self.lt = np.asarray(self.lt) + 48.0
+
+        # Cycle through float and array-like input
+        for is_float in [True, False]:
+            if is_float:
+                args = [self.lt[-1], self.pole_lt[-1], self.pole_ang[-1]]
+                self.comp = np.asarray(3)
+            else:
+                args = [self.lt, self.pole_lt, self.pole_ang]
+                self.comp = np.array([1, 2, 2, 4, 1, 4, 2, 3])
+
+            with self.subTest(args=args):
+                # Get the output
+                self.out = vectors.define_pole_quadrants(*args)
+
+                # Test the integer quadrant assignment
+                self.assertTupleEqual(self.out.shape, self.comp.shape)
+                self.assertTrue(np.all(self.out == self.comp),
+                                msg="{:} != {:}".format(self.out, self.comp))
+        return
+
+    def test_define_pole_quadrants_large_diff(self):
+        """Test the LT quadrant ID for the dest pole with very pos LT diffs."""
+        self.pole_lt = np.asarray(self.pole_lt) + 48.0
+
+        # Cycle through float and array-like input
+        for is_float in [True, False]:
+            if is_float:
+                args = [self.lt[-1], self.pole_lt[-1], self.pole_ang[-1]]
+                self.comp = np.asarray(3)
+            else:
+                args = [self.lt, self.pole_lt, self.pole_ang]
+                self.comp = np.array([1, 2, 2, 4, 1, 4, 2, 3])
+
+            with self.subTest(args=args):
+                # Get the output
+                self.out = vectors.define_pole_quadrants(*args)
+
+                # Test the integer quadrant assignment
+                self.assertTupleEqual(self.out.shape, self.comp.shape)
+                self.assertTrue(np.all(self.out == self.comp),
+                                msg="{:} != {:}".format(self.out, self.comp))
+        return
+                
         
