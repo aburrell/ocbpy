4d0f1451100df41d4b9d90a4b513ff3e3b284c86
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2024-05-08 13:57:37 -0400

    DOC: updated main headers
    Updated the headers in the main directory code.

--- ocbpy/_boundary.py
+++ ocbpy/_boundary.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# DOI: 10.5281/zenodo.1179230
 # Full license can be found in License.md
+#
+# DISTRIBUTION STATEMENT A: Approved for public release. Distribution is
+# unlimited.
 # ----------------------------------------------------------------------------
 """Hold, manipulate, and load the OCB and EAB data.
 
